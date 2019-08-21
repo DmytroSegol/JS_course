9ba718c03228cc32eb043f82ff66ad35bd9fec79
DmytroSegol <dmytrys457@gmail.com>
2019-08-21 21:25:30 +0300

    first commit

---
+++ scipttest.js
@@ -0,0 +1,35 @@
+// alert(1);
+// //let answer = confirm("Are you here");
+// // console.log(second);
+// var leftBorderWidth = 15;
+// let second = 2;
+// const pi = 3.14;
+// //console.log(answer);
+// let arr = ['plum.png','orange.png','apple.bmp'];
+// console.log(arr[0]);
+
+// //confirm ("Are you here?");
+// let answer = +prompt("есть ли Вам 18","да");
+
+// console.log(typeof(answer));
+
+// console.log("arr "+" - object");
+// console.log(4 + +"- object");
+
+let incr = 10,
+    decr = 10;
+
+
+console.log(incr++);
+console.log(decr--);
+console.log(5%2);
+console.log("2" === 2);
+
+let isChecked = true,
+    isClose = false;
+
+
+    console.log(isChecked || !isClose);
+
+
+
