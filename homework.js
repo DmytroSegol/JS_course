37b1f6f137133b8cd712686d6ceb64ee24ef3f28
DmytroSegol <dmytrys457@gmail.com>
2019-08-29 19:43:15 +0300

    addedHometask

---
+++ homework.js
@@ -0,0 +1,24 @@
+//let money = prompt("Ваш бюджет на месяц");
+//let time = prompt("Введите данные в форма", "YYYY-MM-DD");
+//let appData = {
+//     buget: money,
+//     timeData: time
+
+// };
+// let expenses = {
+//     money: time
+// };
+
+// let optionalExpenses = {
+
+// };
+
+// let timeData;
+
+let income = [];
+
+let savings = false;
+
+let statyaRashodov = prompt("Vvedite obyazatelnuju statju rasxodov");
+let voSkolkoOboydesya = prompt("Vo skolko oboydetsya");
+alert("bydjet na 1 den: "+voSkolkoOboydesya/30);
