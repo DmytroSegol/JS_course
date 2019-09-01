ee5149a7c1cd8e2fb762cd0be4f8ce6e793536eb
DmytroSegol <dmytrys457@gmail.com>
2019-09-01 15:37:13 +0300

    functions

---
+++ Functions/script.js
@@ -0,0 +1,41 @@
+// let num = 20;
+
+// function ShowFirstMessage(text) {
+//     alert(text);
+//     let num = 10;
+//     console.log(num);
+// }
+
+// ShowFirstMessage("HelloWorld");
+// console.log(num);
+
+
+// let calc = function (a,b) {
+//     return (a + b);
+// };
+
+let calc = (a,b) => a+b 
+console.log(calc(3,4));
+
+console.log(calc(8,4));
+
+function retVar() {
+    let num = 50;
+    return num;
+}
+
+let anotherNum = retVar ();
+console.log(anotherNum);
+
+let str = "test";
+console.log(str.length);
+
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+
+let twelve = "12.2";
+
+console.log(Math.round(twelve));
+
+
+
